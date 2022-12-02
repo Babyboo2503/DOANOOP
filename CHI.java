bdac310a3fa427fa673ec5af199173dd1a99c034
Phatnotfat <114798871+Phatnotfat@users.noreply.github.com>
2022-12-02 14:31:48 +0700

    Update CHI.java

--- CHI.java
+++ CHI.java
@@ -1,54 +1,37 @@
 
+
 package com.mycompany.doanhthu;
 import java.util.Scanner;
 
 public class CHI {
     private double dien=3000;
-    private int ki;
     private double nuoc=9000;
-    private int khoi;
     private long matbang=2000000;
-    private double luongnhanvien=0;// tien luong cua nhanvien
-    private double tiennhaphang=0; /// tien nhap hang
+    private double luongnhanvien;// tien luong cua nhanvien
+    private double tiennhaphang; /// tien nhap hang
 
-    public CHI(int ki, int khoi) {
-        this.ki = ki;
-        this.khoi = khoi;
+    public CHI() {
     }
-    public CHI(){
-        this.ki=0;
-        this.khoi=0;
-    }
+
+    
+
+    
     public double getDien() {
-        return dien;
+        return dien*10;
     }
 
     public void setDien(double dien) {
         this.dien = dien;
     }
 
-    public int getKi() {
-        return ki;
-    }
 
-    public void setKi(int ki) {
-        this.ki = ki;
-    }
 
     public double getNuoc() {
-        return nuoc;
+        return nuoc*10;
     }
 
     public void setNuoc(double nuoc) {
         this.nuoc = nuoc;
-    }
-
-    public int getKhoi() {
-        return khoi;
-    }
-
-    public void setKhoi(int khoi) {
-        this.khoi = khoi;
     }
 
     public long getMatbang() {
@@ -60,7 +43,8 @@ public class CHI {
     }
 
     public double getLuongnhanvien() {
-        return luongnhanvien;
+        DOCFILE t= new DOCFILE();
+        return t.luongnhanvien();
     }
 
     public void setLuongnhanvien(double luongnhanvien) {
@@ -68,7 +52,9 @@ public class CHI {
     }
 
     public double getTiennhaphang() {
-        return tiennhaphang;
+        DOCFILE a= new DOCFILE();
+        
+        return a.tienhanghoa();
     }
 
     public void setTiennhaphang(double tiennhaphang) {
@@ -83,19 +69,14 @@ public class CHI {
         this.sc = sc;
     }
     Scanner sc= new Scanner(System.in);
-    public void nhapdichvu(){
-        System.out.println("NHAP GIA TRI TIEU DUNG TRONG THANG");
-        System.out.println("Nhap so ki dien ");
-        ki=sc.nextInt();
-        System.out.println("Nhap so khoi nuoc ");
-        khoi=sc.nextInt();
-    }
     public double phidichvu(){
-        return (double)ki*dien+khoi*nuoc+matbang;
+        return (double)(getDien()+getNuoc()+getMatbang());
     }
     public double tienchi(){
-        return phidichvu()+luongnhanvien+tiennhaphang;
+        return phidichvu()+getLuongnhanvien()+getTiennhaphang();
     }
+
   
 }
+
 
