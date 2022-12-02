14996d96793dfe95e283cac6caf2d0ce37623ec9
Phatnotfat <114798871+Phatnotfat@users.noreply.github.com>
2022-12-02 09:20:12 +0700

    còn thíu giá trị

---
+++ DOANHTHU.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.doanhthu;
+
+
+public class DOANHTHU {
+    private double giatridonhang;
+    //tien thu duoc thu hoa don
+    public double tienthu(){
+        giatridonhang=10000;
+        return giatridonhang;
+    }
+}
---
+++ LOINHUAN.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.doanhthu;
+
+
+public class LOINHUAN {
+    protected double tienloi;
+    public static void main(String[] args) {
+        
+        CHI c1= new CHI();
+        c1.nhapdichvu();
+        System.out.println("---XUAT BANG CHI TIEU TRONG THANG---");
+        System.out.println("Chi : "+c1.tienchi());
+        DOANHTHU d1= new DOANHTHU();
+        System.out.println("Thu : "+d1.tienthu());
+        LOINHUAN l1= new LOINHUAN();
+        l1.tienloi=d1.tienthu()-c1.tienchi();
+        System.out.println("LOI NHUAN THANG :"+l1.tienloi);
+    }
+}
